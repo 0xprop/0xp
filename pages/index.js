2d33d1346d333cad80245a2e283090cc347d9ae1
gabriel miron <jgabrielmiron@gmail.com>
2024-09-06 11:41:38 +0800

    Updated title

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>My First Website</title>
+        <title>My awesome website</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
